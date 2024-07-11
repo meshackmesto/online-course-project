8a6f84cd43e4f7d2f6891386c3133855ed14409a
meshackmesto <meshackorina93@gmail.com>
2024-07-11 08:01:38 +0300

    done

--- server/models.py
+++ server/models.py
@@ -1,20 +1,6 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-from datetime import datetime
 
 from config import db
 
 # Models go here!
-class Student(db.Model):
-    __tablename__ = 'student'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime,)
-    updated_at = db.Column(db.DateTime, default=datetime, onupdate=datetime.utcnow)
-    enrollments = db.relationship('Enrollment', back_populates='student', lazy=True)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
