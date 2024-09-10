4381c48d479dc9498f0e956c5b1a395c8c723757
student <kmac.kimaru93@gmail.com>
2024-09-10 14:37:16 +0300

    rectifying static files path in config.py

--- server/config.py
+++ server/config.py
@@ -10,7 +10,7 @@ from flask_bcrypt import Bcrypt
 import os
 
 # Instantiate app, set attributes
-app = Flask(__name__, static_folder="online-course-project/build", static_url_path="/")
+app = Flask(__name__, static_folder="online-course-project/client/build", static_url_path="/")
 CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SECRET_KEY'] = 'f6f44f8f6f8d86f41eadf3e47791647c54c36bc297b3d60a'
