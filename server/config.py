eb4749fa84d1665d992c072b0866fe9396450409
student <kmac.kimaru93@gmail.com>
2024-09-10 17:12:51 +0300

    reverting to old secret key

--- server/config.py
+++ server/config.py
@@ -7,16 +7,14 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_restful import Api
 from flask_bcrypt import Bcrypt
-import secrets
 import os
 
 print(os.getcwd())
 # Instantiate app, set attributes
-secret_key = secrets.token_hex(32)
 app = Flask(__name__, static_folder="online-course-project/client/build", static_url_path="/")
 CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/app.db'
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key_if_not_set')
+app.config['SECRET_KEY'] = 'f6f44f8f6f8d86f41eadf3e47791647c54c36bc297b3d60a'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json_encoder.compact = False
 
