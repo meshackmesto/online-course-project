2c4ce6a9034b2d93daa6311273e0da0eb0e9df24
student <kmac.kimaru93@gmail.com>
2024-09-10 17:04:19 +0300

    generating  secret key in config.py

--- server/config.py
+++ server/config.py
@@ -7,10 +7,12 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_restful import Api
 from flask_bcrypt import Bcrypt
+import secrets
 import os
 
 print(os.getcwd())
 # Instantiate app, set attributes
+secret_key = secrets.token_hex(32)
 app = Flask(__name__, static_folder="online-course-project/client/build", static_url_path="/")
 CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/app.db'
