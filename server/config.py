c155fbec84cb6fd1ca4e9c468a4484e1583f22d3
student <kmac.kimaru93@gmail.com>
2024-09-10 14:04:12 +0300

    modigying app and config files

--- server/config.py
+++ server/config.py
@@ -7,11 +7,12 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_restful import Api
 from flask_bcrypt import Bcrypt
+import os
 
 # Instantiate app, set attributes
 app = Flask(__name__, static_folder="online-course-project/build", static_url_path="/")
 CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("'DATABASE_URL')")
 app.config['SECRET_KEY'] = 'f6f44f8f6f8d86f41eadf3e47791647c54c36bc297b3d60a'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json_encoder.compact = False
