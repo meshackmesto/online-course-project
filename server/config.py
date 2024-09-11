239e12811a6083d9b5a26f17e6eadbcc503a8299
student <kmac.kimaru93@gmail.com>
2024-09-11 21:29:25 +0300

    changing database uri to absolute path

--- server/config.py
+++ server/config.py
@@ -13,7 +13,8 @@ print(os.getcwd())
 # Instantiate app, set attributes
 app = Flask(__name__, static_folder="online-course-project/client/build", static_url_path="/")
 CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "instance", "app.db")}'
 app.config['SECRET_KEY'] = 'f6f44f8f6f8d86f41eadf3e47791647c54c36bc297b3d60a'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json_encoder.compact = False
